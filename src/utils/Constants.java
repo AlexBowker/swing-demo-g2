bf2ef7598945b3c4a2bfc08a0bafbb1b11b13457
Alex Bowker <alex.bowker@salfordcc.ac.uk>
2025-04-03 16:41:04 +0100

    Add transparent background to missile

--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -13,7 +13,7 @@ public final class Constants {
     public static final int PLAYER_HEIGHT = 80;
     public static final int PLAYER_SPEED = 10;
 
-    public static final String MISSILE_IMAGE_PATH = "resources/missile.jpg";
+    public static final String MISSILE_IMAGE_PATH = "resources/missile.png";
     public static final int MISSILE_WIDTH = 200;
     public static final int MISSILE_HEIGHT = 50;
     public static final int MISSILE_SPEED = 1;
